cd52a3a8dc7f4b9ea2ed284f822cc4f5b3ebfb4b
abbaty <abbaty48@gmail.com>
2026-02-11 13:32:33 +0100

    Setup tailwindcss and initialize shadcn.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import path from "node:path";
@@ -10,6 +11,7 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
   server: {
     host: true,
